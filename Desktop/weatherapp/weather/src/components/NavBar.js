d8be0b1306e541d30242e2fbcf53938fc0c6ec58
bautistax <jbautistabauza@gmail.com>
2023-09-25 18:24:10 -0300

    Navbar

---
+++ Desktop/weatherapp/weather/src/components/NavBar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+ return (
+    <nav className="navbar bg-slate-900 text-lime-50 mb-5">
+        <div className="content-center">
+            <h3 className="mx-auto text-center">Wheather App</h3>
+        </div>
+
+    </nav>
+ );
+};
+
+export default Navbar;
