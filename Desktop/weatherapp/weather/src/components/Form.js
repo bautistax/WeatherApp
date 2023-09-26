7fd5c9a8363c966437ccc5e1d7df91724c6de69c
bautistax <jbautistabauza@gmail.com>
2023-09-26 20:28:47 -0300

    WeatherPanel

--- Desktop/weatherapp/weather/src/components/Form.js
+++ Desktop/weatherapp/weather/src/components/Form.js
@@ -3,12 +3,22 @@ import React, {useState} from 'react';
 const Form = () => {
      const [city, setCity] = useState('');
 
+     const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(city);
+        if(city === "" || !city) return;
+     };
+
+     const handleInputChange = (e) => {
+        setCity(e.target.value);
+     };
+
      return (
-        <div className=''>
-            <form  className=''>
-                <div className=''>
-                    <input type='text' className='' placeholder='City' />
-                    <button className='btn' type='submit'>Seacrh</button>
+        <div className='container'>
+            <form onSubmit={handleSubmit}>
+                <div className='mx-auto mb-3'>
+                    <input type='text' className='' placeholder='City' onChange={handleInputChange} />
+                    <button className=' bg-blue-700 text-black' type='submit'>Search</button>
                 </div>
                 
             </form>
