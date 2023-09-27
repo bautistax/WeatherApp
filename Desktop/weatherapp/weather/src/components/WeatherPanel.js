c94d5fffb72c3c336c4750c7f61dab329ea472fa
bautistax <jbautistabauza@gmail.com>
2023-09-27 18:13:59 -0300

    WeatherPanel.js

--- Desktop/weatherapp/weather/src/components/WeatherPanel.js
+++ Desktop/weatherapp/weather/src/components/WeatherPanel.js
@@ -1,1 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+
+const WeatherPanel = () => {
+
+    const urlWeather = "https://api.openweathermap.org/data/2.5/weather?appid=798e111e9f5e90920f4ea6a4408f4f80&lang=en";
+    const cityUrl = "&q=";
+
+    const urlForecast = "https://api.openweathermap.org/data/2.5/forecast?appid=798e111e9f5e90920f4ea6a4408f4f80&lang=en";
+
+    const [ weather, setWeather ] = useState([]);
+    const [forecast, setForecast ] = useState([]);
+};
+
+export default WeatherPanel;
