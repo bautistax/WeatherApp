b142549c0c6aa50ee38a23391c0d5f7c157827bd
bautistax <jbautistabauza@gmail.com>
2023-09-27 19:44:27 -0300

    Apiaxios

--- Desktop/weatherapp/weather/src/components/WeatherPanel.js
+++ Desktop/weatherapp/weather/src/components/WeatherPanel.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const WeatherPanel = () => {
 
@@ -7,8 +8,72 @@ const WeatherPanel = () => {
 
     const urlForecast = "https://api.openweathermap.org/data/2.5/forecast?appid=798e111e9f5e90920f4ea6a4408f4f80&lang=en";
 
-    const [ weather, setWeather ] = useState([]);
-    const [forecast, setForecast ] = useState([]);
+    const [weather, setWeather] = useState([]);
+    const [forecast, setForecast] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [show, setShow] = useState(false);
+    const [location, setLocation] = useState("");
+
+    const getLocation = async (loc) => {
+        setLoading(true);
+        setLocation(loc);
+
+        //weather
+        urlWeather = urlWeather + cityUrl + loc;
+
+        // Utiliza axios para realizar la solicitud HTTP
+        axios.get(urlWeather)
+            .then((response) => {
+                // Verifica si la respuesta no es exitosa y lanza un error si es necesario
+                if (!response.data) {
+                    throw new Error('No se encontraron datos de clima');
+                }
+
+                // Procesa los datos de clima
+                const weatherData = response.data;
+                console.log(weatherData);
+
+                // Actualiza el estado con los datos de clima
+                setWeather(weatherData);
+            })
+            .catch((error) => {
+                console.error(error);
+
+                // Realiza las acciones necesarias cuando se produce un error
+                setLoading(false);
+                setShow(false);
+            });
+
+        //forecast
+
+        urlForecast = urlForecast + cityUrl + loc;
+
+        axios.get(urlForecast)
+            .then((response) => {
+                // Verifica si la respuesta no es exitosa y lanza un error si es necesario
+                if (!response.data) {
+                    throw new Error('No se encontraron datos de clima');
+                }
+
+                // Procesa los datos de clima
+                const forecastData = response.data;
+                console.log(forecastData);
+
+                // Actualiza el estado con los datos de clima
+                setForecast(forecastData);
+            })
+            .catch((error) => {
+                console.error(error);
+
+                // Realiza las acciones necesarias cuando se produce un error
+                setLoading(false);
+                setShow(false);
+            });
+
+
+
+
+    }
 };
 
 export default WeatherPanel;
