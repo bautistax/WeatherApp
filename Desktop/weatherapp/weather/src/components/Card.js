688543ea9018af1cb52a868224f2e86704ede5c4
bautistax <jbautistabauza@gmail.com>
2023-09-29 19:44:12 -0300

    Card style

--- Desktop/weatherapp/weather/src/components/Card.js
+++ Desktop/weatherapp/weather/src/components/Card.js
@@ -3,9 +3,24 @@ import Loading from "./Loading/Loading.js";
 
 const Card = ({loadingData, showData, weather, forecast}) => {
 
+    let today = new Date();
+    let day = today.getDay();
+    let month = today.getMonth() + 1;
+    let year = today.getFullYear();
+    let date = day + '/' + month + '/' + year;
+
+    let url = "";
+    let urlIcon = "";
+
+
     if(loadingData){
         return <Loading />
-    }
+    };
+
+    if(showData){
+        url = "http://openweathermap.org/img/w/";
+        urlIcon = url + weather.weather[0].icon + ".png";
+    };
 
     return (
         <div className="mt-5">
@@ -16,7 +31,11 @@ const Card = ({loadingData, showData, weather, forecast}) => {
                         <div className="mb-3 mx-auto bg-black text-lime-50">
                             <div className="row gap-0">
                                 <div className="">
-                                    <img src="https://images.pexels.com/photos/2097616/pexels-photo-2097616.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+                                    <h3>{weather.name}</h3>
+                                    <p>{date}</p>
+                                    <h1>{(weather.main.temp - 273.15).toFixed(1)}°C</h1>
+                                    <p><img src={urlIcon} alt="icon" />{weather.weather[0].description}</p>
+                                    <img src="https://images.pexels.com/photos/2097616/pexels-photo-2097616.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="city" />
 
                                 </div>
                                 <div className="">
