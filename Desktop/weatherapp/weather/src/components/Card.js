151b3e17143b9e32559bdf87d91e1064d0193d2e
bautistax <jbautistabauza@gmail.com>
2023-10-02 17:55:15 -0300

    design

--- Desktop/weatherapp/weather/src/components/Card.js
+++ Desktop/weatherapp/weather/src/components/Card.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Loading from "./Loading/Loading.js";
+import '../App.css';
 
 const Card = ({loadingData, showData, weather, forecast}) => {
 
@@ -28,18 +29,19 @@ const Card = ({loadingData, showData, weather, forecast}) => {
             {
                 showData === true ? (
                     <div className="container">
-                        <div className="mb-3 mx-auto bg-black text-lime-50">
-                            <div className="row gap-0">
-                                <div className="">
-                                    <h3>{weather.name}</h3>
-                                    <p>{date}</p>
-                                    <h1>{(weather.main.temp - 273.15).toFixed(1)}°C</h1>
+                        <div className="card mb-3 mx-auto bg-black text-lime-50">
+                            <div className="gap-0">
+                                <div className="columns-md">
+                                    <h3 className="card-title">{weather.name}</h3>
+                                    <p className="card-date">{date}</p>
+                                    <h1 className="card-temperature">{(weather.main.temp - 273.15).toFixed(1)}°C</h1>
                                     <p><img src={urlIcon} alt="icon" />{weather.weather[0].description}</p>
-                                    <img src="https://images.pexels.com/photos/2097616/pexels-photo-2097616.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="city" />
+                                    <img src="https://i.pinimg.com/564x/88/b8/bb/88b8bb8183ba4e674a058a470a227aee.jpg" alt="city" className="rounded-s" />
 
                                 </div>
-                                <div className="">
-                                    <div className="text-start mt-2">
+                                <div className="columns-md">
+                                    <div className="card-body text-start mt-2">
+                                        <h5 className="card-text">Maximum temperature: {(weather.main.temp_max - 273.15).toFixed(1)}°C</h5>
 
                                     </div>
 
