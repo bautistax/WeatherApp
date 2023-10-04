66c2bb212f4f0a4a00d8e4f1d012f9ae2755a433
bautistax <jbautistabauza@gmail.com>
2023-10-04 20:21:07 -0300

    Card design

--- Desktop/weatherapp/weather/src/components/Card.js
+++ Desktop/weatherapp/weather/src/components/Card.js
@@ -43,55 +43,46 @@ const Card = ({loadingData, showData, weather, forecast}) => {
 
             {
                 showData === true ? (
-                    <div className="container">
-                        <div className="card mb-3 mx-auto bg-black text-lime-50">
-                            <div className="gap-0">
-                                <div className="columns-md">
-                                    <h3 className="card-title">{weather.name}</h3>
-                                    <p className="card-date">{date}</p>
-                                    <h1 className="card-temperature">{(weather.main.temp - 273.15).toFixed(1)}°C</h1>
-                                    <p><img src={urlIcon} alt="icon" />{weather.weather[0].description}</p>
-                                    <img src="https://i.pinimg.com/564x/db/df/c2/dbdfc26503d1b6150202f1563142a02a.jpg" alt="city" className="rounded-s" />
-
-                                </div>
-                                <div className="columns-md">
-                                    <div className="card-body text-start mt-2">
-                                        <h5 className="card-text">Maximum Temperature: {(weather.main.temp_max - 273.15).toFixed(1)}°C</h5>
-                                        <h5 className="card-text">Minimum Temperature: {(weather.main.temp_min - 273.15).toFixed(1)}°C</h5>
-                                        <h5 className="card-text">Feels Like: {(weather.main.feels_like - 273.15).toFixed(1)}°C</h5>
-                                        <h5 className="card-text">Humidity: {weather.main.humidity}%</h5>
-                                        <h5 className="card-text">Wind Speed: {weather.wind.speed}m/s</h5>
-                                    </div>
-                                    <hr/>
-
-                                    <div className="mt-4">
-                                        <div className="col-auto">
-                                            <p>{forecastDate3}h</p>
-                                            <p className="description"><img src={urlIcon3} alt="icon" />{forecast.list[1].weather[0].description}</p>
-                                            <p className="temperature">{(forecast.list[1].main.temp - 273.15).toFixed(1)} °C</p>
-                                        </div>
-
-                                        <div className="col-auto">
-                                            <p>{forecastDate6}h</p>
-                                            <p className="description"><img src={urlIcon6} alt="icon" />{forecast.list[2].weather[0].description}</p>
-                                            <p className="temperature">{(forecast.list[2].main.temp - 273.15).toFixed(1)} °C</p>
-                                        </div>
-
-                                        <div className="col-auto">
-                                            <p>{forecastDate9}h</p>
-                                            <p className="description"><img src={urlIcon9} alt="icon" />{forecast.list[3].weather[0].description}</p>
-                                            <p className="temperature">{(forecast.list[3].main.temp - 273.15).toFixed(1)} °C</p>
-                                        </div>
-
-                                    </div>
-
-                                </div>
-
-                            </div>
-
+                    <div className="mx-auto w-96 h-96 lg:w-120 lg:h-120 relative">
+                    <img src="https://i.pinimg.com/564x/db/df/c2/dbdfc26503d1b6150202f1563142a02a.jpg" alt="city" className="rounded-s w-full h-full lg:w-full lg:h-full object-cover" />
+                    <div className="absolute inset-0 flex flex-col justify-between p-4 text-white">
+                        <div>
+                            <h3 className="text-2xl font-semibold">{weather.name}</h3>
+                            <p className="text-sm">{date}</p>
+                            <h1 className="text-4xl">{(weather.main.temp - 273.15).toFixed(1)}°C</h1>
+                            <p className="text-sm"><img src={urlIcon} alt="icon" />{weather.weather[0].description}</p>
                         </div>
-
+                        <div className="card-body text-start mt-2">
+                            <h5 className="text-sm">Max Temp: {(weather.main.temp_max - 273.15).toFixed(1)}°C</h5>
+                            <h5 className="text-sm">Min Temp: {(weather.main.temp_min - 273.15).toFixed(1)}°C</h5>
+                            <h5 className="text-sm">Feels Like: {(weather.main.feels_like - 273.15).toFixed(1)}°C</h5>
+                            <h5 className="text-sm">Humidity: {weather.main.humidity}%</h5>
+                            <h5 className="text-sm">Wind Speed: {weather.wind.speed}m/s</h5>
+                        </div>
+                        <hr className="my-2" />
+                        <div className="mt-2 flex justify-between">
+                            <div className="col-auto">
+                                <p className="text-sm">{forecastDate3}h</p>
+                                <p className="text-sm description"><img src={urlIcon3} alt="icon" />{forecast.list[1].weather[0].description}</p>
+                                <p className="text-sm temperature">{(forecast.list[1].main.temp - 273.15).toFixed(1)} °C</p>
+                            </div>
+                            <div className="col-auto">
+                                <p className="text-sm">{forecastDate6}h</p>
+                                <p className="text-sm description"><img src={urlIcon6} alt="icon" />{forecast.list[2].weather[0].description}</p>
+                                <p className="text-sm temperature">{(forecast.list[2].main.temp - 273.15).toFixed(1)} °C</p>
+                            </div>
+                            <div className="col-auto">
+                                <p className="text-sm">{forecastDate9}h</p>
+                                <p className="text-sm description"><img src={urlIcon9} alt="icon" />{forecast.list[3].weather[0].description}</p>
+                                <p className="text-sm temperature">{(forecast.list[3].main.temp - 273.15).toFixed(1)} °C</p>
+                            </div>
+                        </div>
                     </div>
+                </div>
+                
+                
+                
+                
                 ):(
                     <h2 className="text-center mx-auto text-lime-50">No data available</h2>
                 )
